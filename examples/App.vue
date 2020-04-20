947cd4d332eb1b292de6dacf743e90309ca61139
luopc <luopc1218@outlook.com>
2020-04-20 09:49:55 +0800

    修改示例  添加实例gif

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="app">
         <l-scrollText style="width:100rem;height: 10rem;border:1px solid #ff0000">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid deserunt ea eum facere illo ipsum non obcaecati pariatur similique? Numquam possimus ratione vitae. Adipisci, dolorem error esse ipsam molestias nihil quaerat soluta? A consequatur cum dolore dolores ex exercitationem facilis iste laboriosam magni, molestiae molestias nulla odio odit omnis pariatur ratione repudiandae rerum soluta totam voluptatibus! Aliquid aperiam architecto aut cum debitis, est exercitationem fuga harum hic ipsa iure magni modi molestias nesciunt nisi numquam obcaecati odio provident quidem quos reprehenderit sunt unde ut velit voluptas. Alias aut consectetur cum, facere iste quo saepe! Asperiores iusto perferendis possimus provident.
         </l-scrollText>
     </div>
 </template>
